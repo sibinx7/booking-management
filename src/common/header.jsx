5f3cfcdef7e2cb5c5eae00cda17082bbab26eee2
Sibin Xavier <sibinx7@gmail.com>
2020-03-16 13:05:48 +0530

    UI fix for slider

--- src/common/header.jsx
+++ src/common/header.jsx
@@ -32,19 +32,57 @@ export const MainHeader = () => {
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon" />
         </button>
-        <div className="d-none d-block-md">
-        <button className="btn btn-default">
-          <span className="fas fa-search"></span>
-        </button>
-        <a href="">
-          <img src={headerLogo} alt="Main header logo"/>
-        </a>
-        <button type="" className="">
+        <div className="d-lg-none d-mobile-only-header">
+        <div className="mobile-left-block">
+        <form action="" onSubmit={(e) => handleSubmit(e)}>
+                <div className="input-group">
+                  {
+                    activeForm &&  <input type="text" className="form-control"/>
+                  }                  
+                  <div className="input-group-append">
+                   {
+                     activeForm && (
+                       <button classname="btn btn-default" onClick={e => resetSearchForm(e)}>
+                         <span className="fa fa-times"></span>
+                       </button>
+                     )
+                   } 
+                  <button className="btn btn-default" onClick={e => handleSearchButtonClick(e)}>
+                    <span className="fa fa-search"></span>
+                  </button>
+                  </div>
+                </div>
+              </form>
+        </div>
+        <div className="mobile-center-block">
+          <a href="">
+            <img src={headerLogo} alt="Main header logo"/>
+          </a>
+        </div>        
+        <div className="mobile-right-block">
+        <a type="" className="btn btn-spl-default notification-present">
+        <span className="notification--red circle"></span>
           <span className="fas fa-comment"></span>
-        </button>
-        <button>
+        </a>
+        <div className="dropdown dropdown-left-link d-inline-block">
+        <a className="btn btn-spl-default notification-present" href="#" data-toggle="dropdown">
+          <span className="notification--red circle"></span>
           <span className="fas fa-bell"></span>
-        </button>
+        </a>  
+        <div className="dropdown-menu dropdown-menu-right dropdown-notification">
+                <div className="dropdown-item">
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque adipisci officia libero suscipit quia optio? Corporis eaque deleniti et dolore, illo vel eveniet at veniam aperiam, similique quasi, dignissimos facere.
+                </div>
+                <div className="dropdown-item">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime perspiciatis reiciendis aut repudiandae officia harum dolorem voluptatem quibusdam voluptas unde consequuntur eum magni animi porro ad ab, temporibus ducimus? Obcaecati?
+                </div>
+                <div className="dropdown-item">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet, aperiam iure minus dignissimos reiciendis aspernatur molestiae earum doloribus beatae delectus esse optio quod nostrum numquam nisi harum, tempora, atque corrupti.
+                </div>
+              </div>
+        </div>
+        
+        </div>        
         </div>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <a className="navbar-brand ml-0 d-none d-lg-block" href="#">
